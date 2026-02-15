b25e94dbea4b5d2cd85ee410c0faf2e08fe43bb5
Ibrahim Hassan <ibrabasm4456@gmail.com>
2026-02-15 22:36:55 +0200

    fixed listen

--- server/server.js
+++ server/server.js
@@ -60,7 +60,12 @@ app.use("/api/status", (req, res) => {
 app.use("/api/auth", userRouter);
 app.use("/api/messages", messageRouter);
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+  });
+}
+
+// export server for vercel
+export default server;
